1dd00d10e595ca44e32b577caa9352f097f1465d
The_Ultimate_Sequeira <117172083+Nathan-sudo-pycharm@users.noreply.github.com>
2024-03-29 23:56:23 +0530

    commit2

--- resetservo.ino
+++ resetservo.ino
@@ -1,16 +1,22 @@
+
 #include <Servo.h>
 
-Servo myservo;  // Create a servo object
+#define servoPin A5 // Servo pin
+
+Servo myservo; // Create a servo object
 
 void setup() {
-  myservo.attach(9);  // Attach the servo to pin 9
+  Serial.begin(9600); // Initialize serial communication
+  myservo.attach(servoPin); // Attach the servo to pin A5
 }
 
 void loop() {
-  resetServo();  // Call the function to reset the servo to 90 degrees
+  resetServo(); // Call the function to reset the servo to 90 degrees
 }
 
 void resetServo() {
-  myservo.write(90);  // Set the servo position to 90 degrees
-  delay(1000);        // Delay to allow the servo to reach the position
+  myservo.write(90); // Set the servo position to 90 degrees
+  Serial.print("Servo position: ");
+  Serial.println(myservo.read()); // Print the current position of the servo
+  delay(600); // Delay to allow the servo to reach the position
 }
