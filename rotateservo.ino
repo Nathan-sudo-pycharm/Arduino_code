f0263c41c4b15b43f418daf07b6256d74d97741b
The_Ultimate_Sequeira <117172083+Nathan-sudo-pycharm@users.noreply.github.com>
2024-03-30 01:05:25 +0530

    commit 3

---
+++ rotateservo.ino
@@ -0,0 +1,32 @@
+
+#include <Servo.h>
+
+#define servoPin A5 // Servo pin
+
+Servo myservo; // Create a servo object
+
+void setup() {
+  myservo.attach(servoPin); // Attach the servo to pin A5
+}
+
+void loop() {
+  // Move to 90 degrees
+  myservo.write(90);
+  delay(5000); // 5 seconds delay
+  delay(2000); // 2 seconds delay
+
+  // Move to 0 degrees
+  myservo.write(0);
+  delay(5000); // 5 seconds delay
+  delay(2000); // 2 seconds delay
+
+  // Move to 180 degrees
+  myservo.write(180);
+  delay(5000); // 5 seconds delay
+  delay(2000); // 2 seconds delay
+
+  // Move back to 90 degrees
+  myservo.write(90);
+  delay(3000); // 3 seconds delay
+}
+
